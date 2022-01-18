a13f1323f77632ef9581780be0e3ebbd72490642
Paulo Santana <paulosantana.sa@gmail.com>
2022-01-17 23:49:20 -0300

    create state class in planet

--- React/02-primeiro-projeto/src/components/planets/index.js
+++ React/02-primeiro-projeto/src/components/planets/index.js
@@ -1,31 +1,69 @@
 import React, { Fragment } from 'react';
 import Planet from './planet';
 
+class Planets extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            planets: [
+                {
+                    name:"Mercúrio",
+                    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi at nobis exercitationem ipsum ipsam magni illum tempora eius nostrum vero sint numquam, odio tenetur quia ut cupiditate quis, possimus optio!",
+                    link:"https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+                    img_url:"https://s2.glbimg.com/HLls-3koR7z8f7OKyeTaFYxBOeI=/512x320/smart/e.glbimg.com/og/ed/f/original/2017/09/22/pia16853mercury.jpg"
+                },
+                {
+                    name:"Plutão",
+                    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi at nobis exercitationem ipsum ipsam magni illum tempora eius nostrum vero sint numquam, odio tenetur quia ut cupiditate quis, possimus optio!",
+                    link:"https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+                    img_url:"https://s2.glbimg.com/HLls-3koR7z8f7OKyeTaFYxBOeI=/512x320/smart/e.glbimg.com/og/ed/f/original/2017/09/22/pia16853mercury.jpg"
+                },
+                {
+                    name:"Mercúrio",
+                    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi at nobis exercitationem ipsum ipsam magni illum tempora eius nostrum vero sint numquam, odio tenetur quia ut cupiditate quis, possimus optio!",
+                    link:"https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+                    img_url:"https://s2.glbimg.com/HLls-3koR7z8f7OKyeTaFYxBOeI=/512x320/smart/e.glbimg.com/og/ed/f/original/2017/09/22/pia16853mercury.jpg"
+                }
+            ]
+        }
+    }
 
 
-const Planets = () => {
-    return(
-        <Fragment>
-            <h3>Planets List</h3>
-            <button>Show message!</button>
-            <hr />
-            <Planet 
-            name="Mercúrio"
-            // description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi at nobis exercitationem ipsum ipsam magni illum tempora eius nostrum vero sint numquam, odio tenetur quia ut cupiditate quis, possimus optio!"
-            link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-            img_url="https://s2.glbimg.com/HLls-3koR7z8f7OKyeTaFYxBOeI=/512x320/smart/e.glbimg.com/og/ed/f/original/2017/09/22/pia16853mercury.jpg"
-            title_with_underline={true}
-            gray={true}
-            /> 
-<hr />
-            <Planet 
-            name="Plutão"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi at nobis exercitationem ipsum ipsam magni illum tempora eius nostrum vero sint numquam, odio tenetur quia ut cupiditate quis, possimus optio!"
-            // link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-            img_url="https://s2.glbimg.com/HLls-3koR7z8f7OKyeTaFYxBOeI=/512x320/smart/e.glbimg.com/og/ed/f/original/2017/09/22/pia16853mercury.jpg"
-            />
-        </Fragment>
-    )
+    removeLast = () => {
+        let new_planets = [...this.state.planets]
+        new_planets.pop()
+        this.setState(state => ({
+            planets: new_planets
+        }))
+    }
+
+
+    duplicateLastPlanet = () => {
+        let last_planet = this.state.planets[this.state.planets.length -1]
+        this.setState(state => ({
+            planets: [...this.state.planets, last_planet]
+        }))
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <h3>Planets List</h3>
+                <button onClick={this.removeLast}>Remove Last</button>
+                <button onClick={this.duplicateLastPlanet}>Duplicate Laste</button>
+                <hr />
+                {this.state.planets.map((planet) =>
+                <Planet 
+                    name={planet.name}
+                    description={planet.description}
+                    link={planet.link}
+                    img_url={planet.img_url}
+                /> 
+                )}
+                <hr />
+            </Fragment>
+        )
+    };
 }
 
 export default Planets;
